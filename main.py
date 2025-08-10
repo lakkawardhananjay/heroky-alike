a10b7919d8c85c26916475bb2d8adba2f665133a
lakkawardhananajay <lakkawardhananjay@gmail.com>
2025-08-11 00:22:07 +0530

    version 2

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 from fastapi import FastAPI
-import random
-app= FastAPI()
+import random 
+from fastapi.responses import HTMLResponse
 
 
-@app.get("/")
-def read_root():
-    return {"message":"🚀 Hello from FastAPI on Kubernetes with ArgoCD!"}
+app= FastAPI(title="Random Number Generator", version="1.0.0")
+
+
+@app.get("/", response_class=HTMLResponse)
+async def root():
+    # Return the HTML content above
+    with open("index.html", "r") as f:
+        return f.read()
 @app.get("/random")
 def random_no():
     return{"number": random.randint(1, 100)}
+
+@app.get("/hi")
+def hi():
+    return{"hi Dhananjay this side !"}
+
+
