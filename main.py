705bd69ebebe9bce8a0470507ed83e24a674953e
lakkawardhananajay <lakkawardhananjay@gmail.com>
2025-08-10 23:52:46 +0530

    verion 2

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
-
+import random
 app= FastAPI()
 
 
 @app.get("/")
 def read_root():
     return {"message":"🚀 Hello from FastAPI on Kubernetes with ArgoCD!"}
+@app.get("/random")
+def random_no():
+    return{"number": random.randint(1, 100)}
